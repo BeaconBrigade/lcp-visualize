ce0153beac5bfec0c575dfcb362020df05764e76
Ryan <beaconbrigade@gmail.com>
2023-05-09 11:41:10 -0230

    Wasm support

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use bevy::prelude::*;
+use bevy_wasm_window_resize::WindowResizePlugin;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "lcp-visualizer".to_string(),
+                canvas: Some("#bevy".to_string()),
                 ..default()
             }),
             ..default()
         }))
+        .add_plugin(WindowResizePlugin)
         .add_plugin(lcp_visualize::RootPlugin)
         .run();
 }
